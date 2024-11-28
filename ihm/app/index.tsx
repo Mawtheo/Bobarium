d0d05599ffa60e85f8327dc8ef375a9b35d19421
max max <zekromgame@gmail.com>
2024-11-28 09:01:05 +0100

    update

--- ihm/app/index.tsx
+++ ihm/app/index.tsx
@@ -2,14 +2,14 @@
  * BTS-CIEL2 :: CLIENT
  */
 
-import { Text, View, Button, StyleSheet} from "react-native";
+import { Text, View, Button, StyleSheet, Image } from "react-native";
 
 // Adresse et port du serveur EV3
 const ADRESSE = "192.168.1.153";
 const PORT = 1664;
 
 export default function Index() {
-  const sendCommand = async (command:string) => {
+  const sendCommand = async (command: string) => {
     try {
       const response = await fetch(`http://${ADRESSE}:${PORT}/${command}`);
       if (response.ok) {
@@ -24,19 +24,22 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Text>Théo et Valérian vous présente Bobarium !</Text>
-      <Button title="Avancer" color="red" onPress={() => sendCommand("avancer")} />
-      <Button title="Reculer" color="red" onPress={() => sendCommand("reculer")} />
-      <Button title="Gauche" color="red" onPress={() => sendCommand("gauche")} />
-      <Button title="Droite" color="red" onPress={() => sendCommand("droite")} />
-
-      <Button title="Stop" onPress={() => sendCommand("stop")} />
-      <Button title="Barre" onPress={() => sendCommand("barre")} />
-      <Button title="Distance" onPress={() => sendCommand("distance")} />
-      <Button title="LED On" onPress={() => sendCommand("led_on")} />
-      <Button title="LED Off" onPress={() => sendCommand("led_off")} />
-      <Button title="Bobarium" onPress={() => sendCommand("bobarium")} />
-      <Button title="Gyro" onPress={() => sendCommand("gyro")} />
+      <View style={styles.mouvement}>
+        <Text>Théo et Valérian vous présente Bobarium !</Text>
+        <Button title="Avancer" color="red" onPress={() => sendCommand("avancer")} />
+        <Button title="Reculer" color="red" onPress={() => sendCommand("reculer")} />
+        <Button title="Gauche" color="red" onPress={() => sendCommand("gauche")} />
+        <Button title="Droite" color="red" onPress={() => sendCommand("droite")} />
+      </View>
+      <View style={styles.commande}>
+        <Button title="Stop" color="blue" onPress={() => sendCommand("stop")} />
+        <Button title="Barre" color="blue" onPress={() => sendCommand("barre")} />
+        <Button title="Distance" color="blue" onPress={() => sendCommand("distance")} />
+        <Button title="LED On" color="blue" onPress={() => sendCommand("led_on")} />
+        <Button title="LED Off" color="blue"onPress={() => sendCommand("led_off")} />
+        <Button title="Bobarium" color="blue" onPress={() => sendCommand("bobarium")} />
+        <Button title="Gyro" color="blue" onPress={() => sendCommand("gyro")} />
+      </View>
     </View>
   );
 }
@@ -44,6 +47,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  mouvement: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  commande: {
+    flex: 2,
+    backgroundColor: 'black',
     justifyContent: "center",
     alignItems: "center",
   }
